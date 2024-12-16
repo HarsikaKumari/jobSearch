1ac0b80b6b637d68d3d8cc57c6532ba59a551d3d
Harsika Kumari <kumariharsika8@gmail.com>
2024-12-16 11:06:48 +0530

    initialize the project with

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import dotenv from "dotenv";
+import connectDB from './utils/db.js'
+dotenv.config({});
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOption = {
+    origin: "http//localhost:5173",
+    credentials: true,
+}
+app.use(cors(corsOption));
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running on port ${PORT}`);
+})
