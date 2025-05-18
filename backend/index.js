40210dd7fcd8a44a91a32914ae9edce76b873d16
Harsika kumari <kumariharsika8@gmail.com>
2025-05-18 21:33:19 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-    origin: "https://harsika-job-search.vercel.app/",
+    origin: "https://harsika-job-search.vercel.app",
     credentials: true,
 }
 app.use(cors(corsOption));
